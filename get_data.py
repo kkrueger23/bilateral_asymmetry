457b2018ba325fffa042eb0aa1ab51b7d724e5cb
kkrueger <kkrueger@ithaca.edu>
2023-04-26 12:48:54 -0400

    get_data used to convert excel sheet to list of dictionaries containing subject data

---
+++ get_data.py
@@ -0,0 +1,9 @@
+# Installed Pandas and openpyxl
+import pandas
+
+def get_dict_data(filename, sheet_name):
+    file = "/Users/kkrueger/desktop/Asymmetry/data/"+ filename
+    excel_data_df = pandas.read_excel(file, sheet_name=sheet_name)
+    return excel_data_df.to_dict(orient='records')
+
+# check
