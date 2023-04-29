4936163fc09010f85882b52e7782f38c724c0b86
kkrueger <kkrueger@ithaca.edu>
2023-04-29 12:49:17 -0400

    Update: Switching from (x,y) pair format to list format in joint measurements

--- test/initial_data_test.py
+++ test/initial_data_test.py
@@ -32,13 +32,13 @@ class TestInitialData(unittest.TestCase):
         subject_object = StepDown(subject1)
 
         # test class using first subject
-        self.assertEqual((31.80302, 33.81476), subject_object.ankle_flexion)
-        self.assertEqual((74.38065, 78.46692), subject_object.knee_flexion)
-        self.assertEqual((0.05599, 0.05317), subject_object.knee_displacement)
-        self.assertEqual((69.10696, 60.16306), subject_object.hip_flexion)
-        self.assertEqual((10.31173960310218, 18.047827590237784), subject_object.ankle_power)
-        self.assertEqual((45.03117354457981, 49.893285437348446), subject_object.knee_power)
-        self.assertEqual((44.65708685231801, 32.058886972413774), subject_object.hip_power)
+        self.assertEqual([31.80302, 33.81476], subject_object.ankle_flexion)
+        self.assertEqual([74.38065, 78.46692], subject_object.knee_flexion)
+        self.assertEqual([0.05599, 0.05317], subject_object.knee_displacement)
+        self.assertEqual([69.10696, 60.16306], subject_object.hip_flexion)
+        self.assertEqual([10.31173960310218, 18.047827590237784], subject_object.ankle_power)
+        self.assertEqual([45.03117354457981, 49.893285437348446], subject_object.knee_power)
+        self.assertEqual([44.65708685231801, 32.058886972413774], subject_object.hip_power)
 
     def test_bwsq(self):
         bwsq_dict = get_dict_data('bwsq.xlsx', 'bwsq_data')
@@ -47,16 +47,16 @@ class TestInitialData(unittest.TestCase):
 
         # test class using first subject
         self.assertEqual(-57.96778,subject_object.rotational_pelvic_tilt)
-        self.assertEqual((25.69942, 23.20493), subject_object.ankle_flexion)
-        self.assertEqual((80.75887, 83.69199), subject_object.knee_flexion)
-        self.assertEqual((14.16273, 5.89156),subject_object.knee_adduction)
-        self.assertEqual((-0.01337, -0.01413), subject_object.knee_displacement)
-        self.assertEqual((74.21773, 81.18118), subject_object.hip_flexion)
-        self.assertEqual((3.09391, -14.0808),subject_object.hip_adduction)
-        self.assertEqual((398.67337, 312.75476),subject_object.foot_weight)
-        self.assertEqual((8.964519330372989, 3.529233537272964), subject_object.ankle_power)
-        self.assertEqual((48.60714940452276, 42.54304517372706), subject_object.knee_power)
-        self.assertEqual((42.42833126510425, 53.92772128899996), subject_object.hip_power)
+        self.assertEqual([25.69942, 23.20493], subject_object.ankle_flexion)
+        self.assertEqual([80.75887, 83.69199], subject_object.knee_flexion)
+        self.assertEqual([14.16273, 5.89156],subject_object.knee_adduction)
+        self.assertEqual([-0.01337, -0.01413], subject_object.knee_displacement)
+        self.assertEqual([74.21773, 81.18118], subject_object.hip_flexion)
+        self.assertEqual([3.09391, -14.0808],subject_object.hip_adduction)
+        self.assertEqual([398.67337, 312.75476],subject_object.foot_weight)
+        self.assertEqual([8.964519330372989, 3.529233537272964], subject_object.ankle_power)
+        self.assertEqual([48.60714940452276, 42.54304517372706], subject_object.knee_power)
+        self.assertEqual([42.42833126510425, 53.92772128899996], subject_object.hip_power)
 
     def test_rllun(self):
         rllun_dict = get_dict_data('rllun.xlsx', 'rllun_data')
@@ -64,11 +64,11 @@ class TestInitialData(unittest.TestCase):
         subject_object = Lunge(subject1)
 
         # test class using first subject
-        self.assertEqual((26.65927, 25.8274), subject_object.ankle_flexion)
-        self.assertEqual((103.05579, 104.97586), subject_object.knee_flexion)
-        self.assertEqual((0.04431, 0.04441), subject_object.knee_displacement)
-        self.assertEqual((82.87424, 79.07224), subject_object.hip_flexion)
-        self.assertEqual((17.69417799951346, 27.024233000782523), subject_object.ankle_power)
-        self.assertEqual((48.146637527229124, 47.745653464403375), subject_object.knee_power)
-        self.assertEqual((34.15918447325741, 25.23011353481411), subject_object.hip_power)
-        self.assertEqual((48.067803737682645, 43.43463214407068), subject_object.foot_weight)
+        self.assertEqual([26.65927, 25.8274], subject_object.ankle_flexion)
+        self.assertEqual([103.05579, 104.97586], subject_object.knee_flexion)
+        self.assertEqual([0.04431, 0.04441], subject_object.knee_displacement)
+        self.assertEqual([82.87424, 79.07224], subject_object.hip_flexion)
+        self.assertEqual([17.69417799951346, 27.024233000782523], subject_object.ankle_power)
+        self.assertEqual([48.146637527229124, 47.745653464403375], subject_object.knee_power)
+        self.assertEqual([34.15918447325741, 25.23011353481411], subject_object.hip_power)
+        self.assertEqual([48.067803737682645, 43.43463214407068], subject_object.foot_weight)
