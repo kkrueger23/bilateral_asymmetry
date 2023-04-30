62d462f38c68a1724922a715010dc4680b7763d8
kkrueger <kkrueger@ithaca.edu>
2023-04-30 14:44:46 -0400

    New: code for bilateral asymmetry equations

--- subject.py
+++ subject.py
@@ -17,7 +17,10 @@ def percent_asymmetry(r_leg, l_leg):
     :param l_leg: measure of left leg in specific motion (angle, power, moment)
     :return: standard percent asymmetry value between the two legs (doesn't account for direction)
     """
-    raise RuntimeError
+    try:
+        return 100 * (r_leg - l_leg) / ((r_leg + l_leg) / 2)
+    except ZeroDivisionError:
+        return ValueError
 
 def percent_bilat_asymmetry(r_leg, l_leg):
     """
@@ -25,7 +28,10 @@ def percent_bilat_asymmetry(r_leg, l_leg):
     :param l_leg: measure of left leg in specific motion (angle, power, moment)
     :return: standard percent bilateral asymmetry value between the two legs (accounts for direction)
     """
-    raise RuntimeError
+    try:
+        return 100*((max(r_leg,l_leg)-min(r_leg,l_leg))/min(r_leg,l_leg))
+    except ZeroDivisionError:
+        return ValueError
 
 def percent_gait_asymmetry1(r_leg, l_leg):
     """
@@ -33,7 +39,10 @@ def percent_gait_asymmetry1(r_leg, l_leg):
     :param l_leg: measure of left leg in specific motion (angle, power, moment)
     :return: standard percent asymmetry value between the two legs (tends to be used during gait analysis)
     """
-    raise RuntimeError
+    try:
+        return 100 * (1 - ((min(r_leg, l_leg) / (max(r_leg, l_leg)))))
+    except ZeroDivisionError:
+        return ValueError
 
 def percent_gait_asymmetry2(r_leg, l_leg):
     """
@@ -41,5 +50,8 @@ def percent_gait_asymmetry2(r_leg, l_leg):
     :param l_leg: measure of left leg in specific motion (angle, power, moment)
     :return: standard percent asymmetry value between the two legs (most conservative used during gait analysis)
     """
-    raise RuntimeError
+    try:
+        return 100*(abs(r_leg-l_leg))/(2*(abs(r_leg+l_leg)))
+    except ZeroDivisionError:
+        return ValueError
 
